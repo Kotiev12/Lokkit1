09eec8c28951c617db6dee95c0977ed6ed755af0
Suleim <kotievsuleym@gmail.com>
2025-02-09 16:30:05 +0300

    feature-create-AppComponent

---
+++ src/app/shared/components/app-component/app-component.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-component',
+  imports: [],
+  template: `
+    <p>
+      app-component works!
+    </p>
+  `,
+  styleUrl: './app-component.component.scss'
+})
+export class AppComponentComponent {
+
+}
