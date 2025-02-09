ff499ca9ef5f7ada87c15e8e903069bbb6006480
Suleim <kotievsuleym@gmail.com>
2025-02-09 20:38:38 +0300

    feat(component): create ProfileComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileComponent } from './shared/components/profile/profile.component'
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [RouterOutlet, ProfileComponent],
+  templateUrl: 'app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
