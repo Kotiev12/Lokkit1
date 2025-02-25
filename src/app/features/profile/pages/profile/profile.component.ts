65191bbc1e1e5fd72dc77d8aa1d044ef2a2d3c0f
Suleim <kotievsuleym@gmail.com>
2025-02-25 18:46:36 +0300

    feat(component):create-profile

--- src/app/features/profile/pages/profile/profile.component.ts
+++ src/app/features/profile/pages/profile/profile.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { UserService } from '../../../../core/services/user.service';
+import { NavbarComponent } from "../../../../shared/components/navbar/navbar.component";
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
