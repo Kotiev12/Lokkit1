0f42930c8d9a8a4b8059335dd9a0169d1bb0b0f5
Suleim <kotievsuleym@gmail.com>
2025-02-26 11:12:21 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { ProfileComponent } from './features/profile/pages/profile/profile.component';
-import { FeedComponent } from './features/post/pages/feed/feed.component';
-import { UserComponent } from "./features/user/pages/user/user.component";
-import { ProfileInfoComponent } from "./features/profile/pages/profile-settings/profile-settings.component";
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, ProfileComponent, FeedComponent, UserComponent, ProfileInfoComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
