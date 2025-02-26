d79813b0715eb06119d8e109b2ee6e8b19c0ea12
Suleim <kotievsuleym@gmail.com>
2025-02-26 18:58:38 +0300

    feat(component): create ui-sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/components/navbar/navbar.component";
 import { ProfileComponent } from "./features/profile/pages/profile/profile.component";
+import { SidebarComponent } from "./shared/components/sidebar/sidebar.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, ProfileComponent],
+  imports: [RouterOutlet, NavbarComponent, ProfileComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
