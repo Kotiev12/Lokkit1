6c9948c7515ec18c883a09c8d2cdc22d878385ba
Suleim <kotievsuleym@gmail.com>
2025-02-26 11:36:59 +0300

    feat(profile): added profile children components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { ProfileComponent } from "./features/profile/pages/profile/profile.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
