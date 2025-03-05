e329ab494b6ac98424c1635b9d519e800685664f
Suleim <kotievsuleym@gmail.com>
2025-03-05 11:19:43 +0300

    (feat): create register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { FeedComponent } from './features/post/pages/feed/feed.component';
 import { UsersComonent } from './features/user/pages/users/users.component';
 import { NotFoundComponent } from './features/not-found/not-found.component';
 import { ProfileComponent } from './features/profile/pages/profile/profile.component';
+import { RegisterComponent } from './features/register/register.component';
 
 export const routes: Routes = [
   {path: 'feed', component: FeedComponent},
+  {path: 'register', component: RegisterComponent},
   {path: '', redirectTo: 'feed', pathMatch: 'full'},
   {path: 'users', component: UsersComonent},
   {path: 'profile/:id', component: ProfileComponent, loadChildren: () => import('./features/profile/pages/profile.routes').then(p => p.profileRoutes)},
--- src/app/features/profile/pages/profile-settings/profile-settings.component.ts
+++ src/app/features/profile/pages/profile-settings/profile-settings.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../../../shared/components/navbar/navbar.component";
+import { InputComponent } from "../../components/input.component";
 
 @Component({
   selector: 'app-profile-settings',
-  imports: [],
+  imports: [InputComponent],
   templateUrl: './profile-settings.component.html',
   styleUrl: './profile-settings.component.scss'
 })
