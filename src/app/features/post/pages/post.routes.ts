5e1c81439a0cbc58f9b72e14a68c8bbe9a45aeee
Suleim <kotievsuleym@gmail.com>
2025-03-14 23:20:02 +0300

    feat(post): added edit-post

--- src/app/features/post/pages/post.routes.ts
+++ src/app/features/post/pages/post.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from "@angular/router";
 import { PostDetailsComponent } from "./post-details/post-details.component";
+import { EditPostComponent } from "../components/edit-post/edit-post.component";
 
 export const postRoutes : Routes = [
 {
    path: ':id/details',
    component: PostDetailsComponent
+},
+{
+  path: ':id/edit',
+  component: EditPostComponent
 }]
